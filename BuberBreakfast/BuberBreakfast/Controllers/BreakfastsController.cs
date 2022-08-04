f7ff3801cdd6a2e584637b917c556e157939f581
nelsonsandoval <nelson.sandoval@gmail.com>
2022-08-04 06:48:20 -0600

    Refactoring Finished

--- BuberBreakfast/BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/BuberBreakfast/Controllers/BreakfastsController.cs
@@ -19,16 +19,21 @@ public class BreakfastsController : ApiController
     [HttpPost]
     public IActionResult CreateBreakFast(CreateBreakfastRequest request)
     {
-        var breakfast = new Breakfast(
-            Guid.NewGuid(),
+        ErrorOr<Breakfast> requestToBreakfastResult = Breakfast.Create(
             request.Name,
             request.Description,
             request.StartDateTime,
             request.EndDateTime,
-            DateTime.UtcNow,
             request.Savory,
             request.Sweet
         );
+
+        if (requestToBreakfastResult.IsError)
+        {
+            return Problem(requestToBreakfastResult.Errors);
+        }
+
+        var breakfast = requestToBreakfastResult.Value;
 
         ErrorOr<Created> createBreakfastResult = _breakfastService.CreateBreakfast(breakfast);
 
@@ -53,16 +58,22 @@ public class BreakfastsController : ApiController
     [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakFast(Guid id, UpsertBreakfastRequest request)
     {
-        var breakfast = new Breakfast(
-            id,
+        ErrorOr<Breakfast> requestToBreakfastResult = Breakfast.Create(
             request.Name,
             request.Description,
             request.StartDateTime,
             request.EndDateTime,
-            DateTime.UtcNow,
             request.Savory,
-            request.Sweet
+            request.Sweet,
+            id
         );
+
+        if (requestToBreakfastResult.IsError)
+        {
+            return Problem(requestToBreakfastResult.Errors);
+        }
+
+        var breakfast = requestToBreakfastResult.Value;
 
         ErrorOr<UpsertedBreakfast> upsertedBreakfastResult = _breakfastService.UpsertBreakfast(breakfast);
 
--- BuberBreakfast/BuberBreakfast/Models/Breakfast.cs
+++ BuberBreakfast/BuberBreakfast/Models/Breakfast.cs
@@ -1,29 +1,18 @@
+using BuberBreakfast.ServiceErrors;
+using ErrorOr;
+
 namespace BuberBreakfast.Models;
 
 public class Breakfast
 {
-    public Breakfast()
-    {
-    }
 
-    public Breakfast(Guid id,
-                     string name,
-                     string description,
-                     DateTime startDateTime,
-                     DateTime endDateTime,
-                     DateTime lastModifiedDateTime,
-                     List<string> savory,
-                     List<string> sweet)
-    {
-        Id = id;
-        Name = name;
-        Description = description;
-        StartDateTime = startDateTime;
-        EndDateTime = endDateTime;
-        LastModifiedDateTime = lastModifiedDateTime;
-        Savory = savory;
-        Sweet = sweet;
-    }
+    public const int MinNameLength = 3;
+
+    public const int MaxNameLength = 50;
+
+    public const int MinDescriptionLength = 50;
+
+    public const int MaxDescriptionLength = 150;
 
     public Guid Id { get; }
 
@@ -35,10 +24,71 @@ public class Breakfast
 
     public DateTime EndDateTime { get; }
 
-    public DateTime LastModifiedDateTime {get;}
+    public DateTime LastModifiedDateTime { get; }
 
     public List<string> Savory { get; }
 
     public List<string> Sweet { get; }
+    
+    private Breakfast(
+        Guid id,
+        string name,
+        string description,
+        DateTime startDateTime,
+        DateTime endDateTime,
+        DateTime lastModifiedDateTime,
+        List<string> savory,
+        List<string> sweet)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        StartDateTime = startDateTime;
+        EndDateTime = endDateTime;
+        LastModifiedDateTime = lastModifiedDateTime;
+        Savory = savory;
+        Sweet = sweet;
+    }
+
+    public static ErrorOr<Breakfast> Create(
+        string name,
+        string description,
+        DateTime startDateTime,
+        DateTime endDateTime,
+        List<string> savory,
+        List<string> sweet,
+        Guid? id = null)
+    {
+
+        List<Error> errors = new();
+
+        if (name.Length is < MinNameLength or > MaxNameLength)
+        {
+            errors.Add(Errors.Breakfast.InvalidName);
+        }
+
+        if (description.Length is < MinNameLength or > MaxNameLength)
+        {
+            errors.Add(Errors.Breakfast.InvalidDescription);
+        }
+
+        if (errors.Count > 0)
+        {
+            return errors;
+        }
+
+        return new Breakfast(
+            id ?? Guid.NewGuid(),
+            name,
+            description,
+            startDateTime,
+            endDateTime,
+            DateTime.UtcNow,
+            savory,
+            sweet
+        );
+
+    }
+
 }
 
